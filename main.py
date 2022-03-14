b873a003181e34fcdea765ba08c495a3d549542f
Om Rawal <omrawal007@gmail.com>
2022-03-14 17:02:06 +0530

    added POST request for API

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, json, jsonify
+from flask import Flask, json, jsonify, request
 from test import chatbot_response
+import ast
 app = Flask(__name__)
 
 
@@ -15,6 +16,24 @@ def called(s):
         "chatbot_response": chatbot_response(s)
     }
     return jsonify(result)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def returnChatbotResponse():
+    if request.method == 'POST':
+        # userQuery = ast.literal_eval(request.args.get('userQuery'))
+        userQuery = (request.args.get('userQuery'))
+        print('############')
+        print(userQuery)
+        print('############')
+        print('type= ', type(userQuery))
+        result = {
+            "user_query": userQuery,
+            "chatbot_response": chatbot_response(userQuery.lower())
+        }
+        return jsonify(result)
+    else:
+        return "<p>Please use proper API POST request call for chatbot response</p>"
 
 
 if __name__ == '__main__':
